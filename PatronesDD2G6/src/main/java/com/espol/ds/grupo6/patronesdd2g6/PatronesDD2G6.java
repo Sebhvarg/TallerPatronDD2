be1891e0a50f77467a9f43f4d813058af603d806
Sebastian <119546442+Sebhvarg@users.noreply.github.com>
2024-07-25 20:50:55 -0500

    Archivos base java

---
+++ PatronesDD2G6/src/main/java/com/espol/ds/grupo6/patronesdd2g6/PatronesDD2G6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.espol.ds.grupo6.patronesdd2g6;
+
+/**
+ *
+ * @author sebas
+ */
+public class PatronesDD2G6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
